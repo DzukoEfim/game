b83ee934a7e10c5610dabb61e76cae6ce20c17a6
Fedoruk Eduard <eduard.fedoruk@nike.com>
2022-10-09 21:31:58 +0300

    remove code repeat

--- src/mechanics/renderer.ts
+++ src/mechanics/renderer.ts
@@ -9,6 +9,8 @@ type ILayers = {
     interfaceLayers: Record<string, ISprite[]>,
 }
 
+type LayerNames = keyof ILayers;
+
 class Renderer {
     private ctx: CanvasRenderingContext2D = null;
     private layers: ILayers = {
@@ -47,66 +49,23 @@ class Renderer {
     }
 
     public draw(): void {
-        this.drawBackgroundLayers();
-        this.drawMapLayers();
-        this.drawObjectsLayers();
-        this.drawinterfaceLayers();
+        this.drawLayer('backgroundLayers');
+        this.drawLayer('mapLayers');
+        this.drawLayer('objectsLayers');
+        this.drawLayer('interfaceLayers');
+        this.drawLayer('interfaceLayers');
         this.resetLayersObject();
     }
 
-    private drawBackgroundLayers(): void {
-        Object.keys(this.layers.backgroundLayers).forEach((layerIndex) => {
-            const tiles = this.layers.backgroundLayers[layerIndex];
-            if (!this.layers.backgroundLayers[layerIndex].length) return;
+    private drawLayer(layerName: LayerNames) {
+        Object.keys(this.layers[layerName]).forEach((layerIndex) => {
+            const tiles = this.layers[layerName][layerIndex];
+            if (!this.layers[layerName][layerIndex].length) return;
 
             tiles.forEach((tile) => {
                 const type = tile.imageType;
                 if (!type) throw new Error('Renderer: missing or unknown type');
 
-                const renderConfiguration = tile.getRenderConfiguration();
-                this.renderByType(type, renderConfiguration);
-            });
-        });
-    }
-
-    private drawMapLayers(): void {
-        Object.keys(this.layers.mapLayers).forEach((layerIndex) => {
-            const tiles = this.layers.mapLayers[layerIndex];
-            if (!this.layers.mapLayers[layerIndex].length) return;
-
-            tiles.forEach((tile) => {
-                const type = tile.imageType;
-                if (!type) throw new Error('Renderer: missing or unknown type');
-
-                const renderConfiguration = tile.getRenderConfiguration();
-                this.renderByType(type, renderConfiguration);
-            });
-        });
-    }
-
-    private drawObjectsLayers(): void {
-        Object.keys(this.layers.objectsLayers).forEach((layerIndex) => {
-            const tiles = this.layers.objectsLayers[layerIndex];
-            if (!this.layers.objectsLayers[layerIndex].length) return;
-
-            tiles.forEach((tile) => {
-                const type = tile.imageType;
-                if (!type) throw new Error('Renderer: missing or unknown type');
-
-                const renderConfiguration = tile.getRenderConfiguration();
-                this.renderByType(type, renderConfiguration);
-            });
-        });
-    }
-
-    private drawinterfaceLayers(): void {
-        Object.keys(this.layers.interfaceLayers).forEach((layerIndex) => {
-            const tiles = this.layers.interfaceLayers[layerIndex];
-            if (!this.layers.interfaceLayers[layerIndex].length) return;
-
-            tiles.forEach((tile) => {
-                const type = tile.imageType;
-                if (!type) throw new Error('Renderer: missing or unknown type');
                 const renderConfiguration = tile.getRenderConfiguration();
                 this.renderByType(type, renderConfiguration);
             });
